e37a69961c560f9369859cfb3ccd803a1e0cc6ff
Kashif Sulehria <captainkashi4@gmail.com>
2023-10-13 01:00:52 +0500

    changes finalized

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './**/*.html',
-    './js/**/*.js',
-  ],
+  content: ["./**/*.html", "./js/**/*.js"],
   theme: {
     extend: {
-      colors:{
-        primary:'#075EAD',
-        yellowish:'#CED643'
-      }
+      colors: {
+        primary: "#F99417",
+        yellowish: "#F99417",
+      },
     },
   },
   plugins: [
     // eslint-disable-next-line global-require
-    require('@tailwindcss/forms'),
+    require("@tailwindcss/forms"),
   ],
-}
+};
